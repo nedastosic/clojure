475a965a9ceac33954b43231b7d67c0930f71bd1
Neda Stosic <nedastossic@gmail.com>
2020-01-05 12:17:13 +0100

    Update dfs to pick a neighbour randomly

--- project/src/project/maze_generator.clj
+++ project/src/project/maze_generator.clj
@@ -1,6 +1,5 @@
 (ns project.maze-generator)
 
-
 (defn generate-matrix [n]
   (def atom-matrix (atom (vec (repeat n (vec (repeat n 0)))))))
 
@@ -54,62 +53,72 @@
   (swap! atom-matrix assoc v1 (assoc (get @atom-matrix v1) v2 0)))
 
 (defn dfs [v]
+  (visit v)
+  (def shuffled [])
   (let [n (count @atom-vector)]
     (loop [i 0]
       (when (< i n)
+        (when (= 1 (get (get @atom-matrix v) i))
+          (def shuffled (conj shuffled i)))
+        (recur (+ i 1))
+        ))
+    (def shuffled (shuffle shuffled))
+    (def shuffled-global (assoc shuffled-global v shuffled))
+    (loop [i 0]
+      (when (< i (count (get shuffled-global v)))
         ;(println i)
-        (visit v)
-        (when (and (= (get @atom-vector i) 0) (= (get (get @atom-matrix v) i) 1))
-          (println (str v "->" i))
-          (remove-wall v i)
-          (remove-wall i v)
-          (dfs i)
+        (when (= (get @atom-vector (get (get shuffled-global v) i)) 0)
+          (println (str v "->" (get (get shuffled-global v) i)))
+          (remove-wall v (get (get shuffled-global v) i))
+          (remove-wall (get (get shuffled-global v) i) v)
+          (dfs (get (get shuffled-global v) i))
           )
         (recur (+ i 1))
         ))
     ))
 
+
 (defn print-maze []
   (let [n (int (Math/sqrt (count @atom-vector)))]
-    (print ".")
-    (apply print (repeat n "__"))
+    (print ".in .")
+    (loop [i 0]
+      (when (< i (- n 1))
+        (print "___.")
+        (recur (+ i 1))))
     (loop [i 0]
       (when (< i n)
         (println)
         (loop [j 0]
           (when (< j n)
 
-            (if (not= -1 (- j 1))
-              (when (= 1 (get (get @atom-matrix (+ (* i n) j)) (- (+ (* i n) j) 1)))
+            (if (= -1 (- j 1)) (print "|"))
+
+            (when (not= -1 (- j 1))
+              (if (= 1 (get (get @atom-matrix (+ (* i n) j)) (- (+ (* i n) j) 1)))
                 (print "|")
+                (print ".")
                 )
-              (print "|")
               )
 
 
             (if (not= n (+ i 1))
               (if (= 1 (get (get @atom-matrix (+ (* i n) j)) (+ (+ (* i n) j) n) 1))
                 (print "___")
-                (print "  "))
-              (print "___"))
+                (print "   "))
+              (if (= n (+ j 1)) (print "out") (print "___")))
 
 
-            (when (= n (+ j 1))
-              (print "|")
-              )
+            (if (= n (+ j 1)) (print "|"))
 
             (recur (+ j 1))))
         (recur (+ i 1))))
     ))
 
 
+(def shuffled-global {})
 (generate-matrix 144)
 (generate-vector 144)
 (initialize)
 
-(dfs 57)
+(dfs 0)
 (print-maze)
-;(println @atom-vector)
-;(println @atom-matrix)
-
-;(println @atom-matrix)
